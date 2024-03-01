9ad68499b1d4b891e6417ff088a0c512c25c9ec7
adam <adamg42685@gmail.com>
2024-02-29 20:13:46 -0500

    css utility class experiments previously talked about

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable tailwindcss/no-custom-classname */
 import React from "react";
 import Image from "next/image";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="min-w-screen min-h-screen bg-black-900">
+    <main className="auth-background">
       <div className="flex-center flex-col">
         <Image
           src="/images/site-logo.png"
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -4,9 +4,7 @@ import { SocialAuthGroup } from "@/components/auth/SocialAuthGroup";
 const SignUpPage = async () => {
   return (
     <div className="flex-center flex-col">
-      <span className="text-left text-2xl leading-8 tracking-[-0.02rem] text-white-100">
-        Create An Account
-      </span>
+      <span className="display-2-bold text-white-100">Create An Account</span>
       <div className="h-[472px] w-[400px]">
         <SignUpForm />
         <SocialAuthGroup />
