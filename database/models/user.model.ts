a0d43be636aa76b7550e018b707f7f72c7dc09c1
adam <adamg42685@gmail.com>
2024-03-03 16:41:49 -0500

    draft of user schema for review

--- database/models/user.model.ts
+++ database/models/user.model.ts
@@ -6,7 +6,21 @@ export interface IUser extends Document {
   password: string;
   createdAt: Date;
   updatedAt: Date;
+  portfolio?: string;
+  learningGoals?: string[];
+  role?: string[];
+  technologyStack?: string[];
+  experienceLevel?: string[];
+  availability?: Date[];
+  tags?: string[];
+  socials?: string[];
+  posts?: string[];
 }
+// User Schema is going to include references to other collections that havent't been created yet,
+//  Object Id type is used to reference other collections
+// TechnologyStack, ExperienceLevel, and Tags are going to be arrays of Object Ids, and Role Based Authentication
+// required is off for now until I get to that implementation.
+// I'm not totally sure if this is correct, but I'm submitting it for Review for feedback
 const UserSchema = new Schema(
   {
     fullname: {
@@ -25,6 +39,44 @@ const UserSchema = new Schema(
     portfolio: {
       type: String,
       required: false,
+    },
+    learningGoals: {
+      type: [String],
+      required: false,
+    },
+    role: {
+      type: [Schema.Types.ObjectId],
+      required: false,
+      default: "user",
+      ref: "Role",
+    },
+    technologyStack: {
+      type: [Schema.Types.ObjectId],
+      required: false,
+      ref: "Technology",
+    },
+    experienceLevel: {
+      type: [Schema.Types.ObjectId],
+      required: false,
+    },
+    availability: {
+      type: [Date],
+      required: false,
+    },
+    tags: {
+      type: [Schema.Types.ObjectId],
+      ref: "Tag",
+      required: false,
+    },
+    socials: {
+      type: [Schema.Types.ObjectId],
+      required: false,
+      ref: "Social",
+    },
+    posts: {
+      type: [Schema.Types.ObjectId],
+      required: false,
+      ref: "Post",
     },
   },
   { timestamps: true }
