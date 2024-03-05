186833c5bd99e3a548de252a3c80f7b0631d5294
adam <adamg42685@gmail.com>
2024-03-05 18:11:09 -0500

    removed extra class

--- components/shared/layout/RightSidebar.tsx
+++ components/shared/layout/RightSidebar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const RightSidebar = () => {
   return (
-    <section className="max-sm:display:none w-72 border-l-[1.5px] bg-black-800 text-white-100 max-lg:hidden">
+    <section className="w-72 border-l-[1.5px] bg-black-800 text-white-100 max-lg:hidden">
       RightSidebar
     </section>
   );
