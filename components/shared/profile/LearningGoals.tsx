b0c626d20bb14159d2709e8f61bddfcd776a0d22
adam <adamg42685@gmail.com>
2024-03-21 10:37:06 -0400

    requested fix on learninggoals

--- components/shared/profile/LearningGoals.tsx
+++ components/shared/profile/LearningGoals.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import LearningGoal from "./LearningGoal";
 import { IUser } from "@/database/models/user.model";
-import { getSession } from "next-auth/react";
-import { getOneUser } from "@/lib/actions/user.actions";
 interface LearningGoalsProps {
   user: Partial<IUser>;
   learningGoals?: {
@@ -15,15 +13,6 @@ export const LearningGoals = async ({
   user,
   learningGoals,
 }: LearningGoalsProps) => {
-  const session = await getSession();
-  if (session) {
-    user = await getOneUser(session.user?.email!);
-  }
-  let cleanUser;
-  if (user && session?.user?.email === user?.email) {
-    cleanUser = JSON.parse(JSON.stringify(user));
-  }
-
   return (
     <div className="w-full px-[30px]">
       {learningGoals?.map((goal, idx) => {
