539621cc85ef4fabfe097f148bef0567dadbc8e7
adam <adamg42685@gmail.com>
2024-04-03 13:23:22 -0400

    fixed spinner , revalidating updated data

--- components/shared/LoadingSpinner.tsx
+++ components/shared/LoadingSpinner.tsx
@@ -4,15 +4,7 @@ const LoadingSpinner = ({ className }: { className?: string }) => {
   return (
     <div className="flex  min-h-[100vh] items-center justify-center">
       <div
-        className="inline-block size-4 animate-spin rounded-full border-4 border-solid border-white-500 align-[-0.125em]  motion-reduce:animate-[spin_1.5s_linear_infinite]"
-        role="status"
-      >
-        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-          Loading...
-        </span>
-      </div>
-      <div
-        className="inline-block size-4 animate-[spinner-grow_0.75s_linear_infinite] rounded-full bg-white-100 align-[-0.125em] opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite]"
+        className="inline-block size-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-black-700 motion-reduce:animate-[spin_1.5s_linear_infinite]"
         role="status"
       >
         <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
