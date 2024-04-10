514452c34df37715cdb59e747552b44e82b0e5fa
adam <adamg42685@gmail.com>
2024-04-10 08:07:31 -0400

    i cant get the syntax to be highlighted , just displayed, i need help

--- components/shared/posts/CodeEditor.tsx
+++ components/shared/posts/CodeEditor.tsx
@@ -17,20 +17,23 @@ import "prismjs/plugins/line-numbers/prism-line-numbers.js";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 import "@/styles/prism.css";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Controller } from "react-hook-form";
+
 interface CodeEditorProps {
   register: any;
   control: any;
+  setValue?: any;
 }
-const CodeEditor = ({ register, control }: CodeEditorProps) => {
-  const [code, setCode] = React.useState("");
-  const codeEditorRef = React.useRef<HTMLTextAreaElement>(null);
-  const generateLineNumbers = () => {
-    const lines = code.split("\n");
-    return lines.map((_, idx) => {
-      return <div key={idx}>{idx + 1} </div>;
-    });
-  };
+const CodeEditor = ({ register, control, setValue }: CodeEditorProps) => {
+  const codeEditorRef = register("code").ref;
+  const codeRef = React.useRef(codeEditorRef);
+  const codeValue = register("code").value;
+  //  const generateLineNumbers = () => {
+  //    const lines = codeValue.split("\n");
+  //    // @ts-ignore
+  //    return lines.map((_, idx) => {
+  //      return <div key={idx}>{idx + 1} </div>;
+  //    });
+  //  };
   useEffect(() => {
     if (codeEditorRef.current) {
       codeEditorRef.current.style.height = "auto";
@@ -39,10 +42,10 @@ const CodeEditor = ({ register, control }: CodeEditorProps) => {
     }
 
     Prism.highlightAll();
-  }, [code]);
+  }, [codeEditorRef, codeValue]);
   return (
     <>
-      <Tabs>
+      <Tabs className="space-y-2">
         {" "}
         <TabsList className="grid w-[222px]  grid-cols-2">
           <TabsTrigger value="code" className="w-1/2">
@@ -54,35 +57,21 @@ const CodeEditor = ({ register, control }: CodeEditorProps) => {
         </TabsList>
         <TabsContent value="code">
           <div className="code-editor-container">
-            <Controller
-              control={control}
-              name="code"
-              render={({ field }) => (
-                <Textarea
-                  contentEditable
-                  id="code"
-                  value={field.value}
-                  onChange={(e) => {
-                    setCode(e.target.value);
-                    field.onChange(e);
-                  }}
-                  className="code-editor-textarea px-3.5 py-3"
-                  spellCheck={false}
-                  ref={codeEditorRef}
-                />
-              )}
+            <Textarea
+              contentEditable
+              id="code"
+              className="code-editor-textarea px-3.5 py-3"
+              spellCheck={false}
+              placeholder="Write your code here"
+              {...register("code")}
             />
-            <div className="code-editor-line-numbers">
-              {generateLineNumbers()}
-            </div>
+
+            <div className="code-editor-line-numbers"></div>
           </div>
         </TabsContent>
         <TabsContent value="preview">
           <pre>
-            <code ref={codeEditorRef} className="language-html">
-              {" "}
-              {code}
-            </code>
+            <code className="language-javascript"> {codeValue}</code>
           </pre>
         </TabsContent>
       </Tabs>
