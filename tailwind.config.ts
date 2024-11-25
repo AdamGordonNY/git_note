8f32ea4b51d8b23aed6d933cc7082ecbd3ef101c
adam <adamg42685@gmail.com>
2024-11-25 13:40:34 -0500

    corrected small typography errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -119,9 +119,7 @@ const config = {
           to: { height: "0" },
         },
       },
-      screens: {
-        xs: "430px",
-      },
+
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
